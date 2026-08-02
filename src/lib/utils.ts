295a1551ec42a116bb60883def8640414db4300b
MR. FICTION <mrfictionbd@gmail.com>
2026-08-02 10:44:44 +0600

    Add layout, globals, utils, prisma, stripe helpers

---
+++ src/lib/utils.ts
@@ -0,0 +1,22 @@
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatPrice(cents: number) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(cents / 100)
+}
+
+export function generateOrderNumber() {
+  const date = new Date()
+  const y = date.getFullYear().toString().slice(-2)
+  const m = (date.getMonth() + 1).toString().padStart(2, '0')
+  const d = date.getDate().toString().padStart(2, '0')
+  const rand = Math.floor(Math.random() * 9000 + 1000)
+  return `MF-${y}${m}${d}-${rand}`
+}
